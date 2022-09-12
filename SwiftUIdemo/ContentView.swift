548f88811fc7351b57f96c9a019e1c52e4b10b19
Peter Cipov <peter@ito.sk>
2022-09-12 09:23:07 +0200

    Initial Commit

---
+++ SwiftUIdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIdemo
+//
+//  Created by Peter Cipov on 12/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIdemo/SwiftUIdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIdemoApp.swift
+//  SwiftUIdemo
+//
+//  Created by Peter Cipov on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
